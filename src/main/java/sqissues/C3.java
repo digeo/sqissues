3da4897ab8c631f34b75e952447d53d1582ea359
gdigas <ma-los-sis@hotmail.com>
2018-11-07 10:46:37 +0200

    C2 Deleted Ce Added with issues: Replace this use of System.out or System.err by a logger.

---
+++ src/main/java/sqissues/C3.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package sqissues;
+
+/**
+ * @author George Digkas <digasgeo@gmail.com>
+ *
+ */
+public class C3 {
+
+	private String s1;
+
+	public C3() {
+	}
+
+	public C3(String s1) {
+		this.s1 = s1;
+	}
+
+	public String getS1() {
+		return s1;
+	}
+
+	public void setS1(String s1) {
+		this.s1 = s1;
+	}
+
+	@Override
+	public String toString() {
+		return "C3 [s1=" + s1 + "]";
+	}
+
+	public void print() {
+		System.out.println("C3 [s1=" + s1 + "]");
+	}
+}
